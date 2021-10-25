e6d7bf064b84691a2f7c0d516899ade06e98d169
sapphirus9 <naoki@sapphirus.biz>
2021-10-25 14:53:30 +0900

    親テーマに合わせ構成を変更

---
+++ src/js/functions.js
@@ -0,0 +1,10 @@
+/**
+ * Theme Name: BlankBlanc Child
+ * Author: Naoki Yamamoto
+ * Description: 共通で使用するJavaScriptです
+ */
+
+// var bbCfgMobileNav = {
+// 	slideNav: [], /* スライドナビ ウイジェットの li が対象（テーマオプションの設定は上書きされます） */
+// 	footerNav: [] /* フッターナビ用 ウイジェットの li が対象 */
+// };
